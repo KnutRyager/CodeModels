759e6593641c3d28be9918b5c904047d80361590
Knut Ryager <e-k-nut@hotmail.com>
2021-12-18 17:29:30 +0100

    Some tree methods for using parent of node during trnasform

--- Common/DataStructures/Trees/Tree.cs
+++ Common/DataStructures/Trees/Tree.cs
@@ -10,12 +10,12 @@ namespace Common.DataStructures
     public class Tree<T> : ITraversableTree<T, Tree<T>>
     {
         public T Data { get; }
-        private readonly Tree<T>[] _children;
+        private readonly List<Tree<T>> _children;
 
         public Tree(T data, IEnumerable<Tree<T>> children)
         {
             Data = data;
-            _children = children.ToArray();
+            _children = new(children ?? Enumerable.Empty<Tree<T>>());
         }
 
         public Tree(params Tree<T>[] valueAndChldren) : this(valueAndChldren[0], valueAndChldren.Skip(1)) { }
@@ -29,9 +29,56 @@ namespace Common.DataStructures
             foreach (var child in node._children) Traverse(child, visitor);
         }
 
+        public Tree<T> AddChild(T dada)
+        {
+            var node = new Tree<T>(dada);
+            _children.Add(node);
+            return node;
+        }
+
         public Tree<T2> Transform<T2>(Func<T, T2> transformer) => new(transformer(Data), _children.Select(x => x.Transform(transformer)));
         public Tree<T2> Transform<T2>(Func<T, IEnumerable<T>, T2> transformer) => new(transformer(Data, _children.Select(x => x.Data)), _children.Select(x => x.Transform(transformer)));
         public T2 To<T2>(Func<T, IEnumerable<T2>, T2> transformer) => transformer(Data, _children.Select(x => x.To(transformer)));
+        public Tree<T2> TransformByParent<T2>(Func<T, T?, T2> transformer)
+        {
+            var result = transformer(Data, default);
+            var tree = new Tree<T2>(result);
+            TransformByParentCollect(tree, transformer);
+            return tree;
+        }
+
+        private void TransformByParentCollect<T2>(Tree<T2> tree, Func<T, T?, T2> transformer)
+        {
+            foreach (var child in _children)
+            {
+                var result = transformer(child, this);
+                child.TransformByParentCollect(tree.AddChild(result), transformer);
+            }
+        }
+
+        public Tree<T2> TransformByTransformedParent<T2>(Func<T, T2?, T2> transformer)
+        {
+            var result = transformer(Data, default);
+            var tree = new Tree<T2>(result);
+            TransformByTransformedParentCollect(tree, transformer);
+            return tree;
+        }
+
+        private void TransformByTransformedParentCollect<T2>(Tree<T2> tree, Func<T, T2?, T2> transformer)
+        {
+            foreach (var child in _children)
+            {
+                var result = transformer(child, tree);
+                child.TransformByTransformedParentCollect(tree.AddChild(result), transformer);
+            }
+        }
+
+        public List<T> ToList()
+        {
+            var result = new List<T>();
+            Traverse(this, x => result.Add(x));
+            return result;
+        }
 
         public static implicit operator T(Tree<T> tree) => tree.Data;
         public static implicit operator Tree<T>(T data) => new(data);
