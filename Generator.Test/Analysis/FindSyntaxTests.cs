98c77ff904ba519788739a10e8cb78bd2c1acc03
Knut Ryager <e-k-nut@hotmail.com>
2021-12-31 00:10:48 +0100

    More test fixes

--- Generator.Test/Analysis/FindSyntaxTests.cs
+++ Generator.Test/Analysis/FindSyntaxTests.cs
@@ -10,43 +10,43 @@ public class FindSyntaxTests
     [Fact]
     public void GetPublicStaticFields()
         => Assert.Equal("public const DateTime variable = null;",
-            "using System; class Test{public const DateTime variable = null;}".Parse().GetPublicStaticFields().First().GetText().ToString());
+            "using System; class Test{public const DateTime variable = null;}".Parse(nameof(GetPublicStaticFields)).GetPublicStaticFields().First().GetText().ToString());
 
     [Fact]
     public void GetConst()
         => Assert.Equal("DateTime variable = null",
-            "using System; class Test{public const DateTime variable = null;}".Parse().GetVariables().First().GetText().ToString());
+            "using System; class Test{public const DateTime variable = null;}".Parse(nameof(GetConst)).GetVariables().First().GetText().ToString());
 
     [Fact]
     public void GetProperties_Simple()
         => Assert.Equal("public DateTime variable { get; set; }",
-            "using System; class Test{public DateTime variable { get; set; }}".Parse().GetProperties().First().GetText().ToString());
+            "using System; class Test{public DateTime variable { get; set; }}".Parse(nameof(GetProperties_Simple)).GetProperties().First().GetText().ToString());
 
     [Fact]
     public void GetProperties_Simple2()
         => Assert.Equal("public DateTime variable { get; set; }",
-            "using System; class Test{public DateTime variable { get; set; }}".Parse().GetProperties().First().GetText().ToString());
+            "using System; class Test{public DateTime variable { get; set; }}".Parse(nameof(GetProperties_Simple2)).GetProperties().First().GetText().ToString());
 
     [Fact]
     public void GetFieldType_System()
         => Assert.Equal("System.DateTime",
             "using System; class Test{public DateTime variable { get; set; }}"
             .Parse(nameof(GetFieldType_System)).GetProperties().First().DescendantNodes()
-            .OfType<IdentifierNameSyntax>().First().GetTType().ToString());
+            .OfType<IdentifierNameSyntax>().First().GetType(nameof(GetFieldType_System)).ToString());
 
     [Fact]
     public void GetFieldType_System_List()
         => Assert.Equal("System.Collections.Generic.List<int>",
             "using System; using System.Collections.Generic; class Test{public List<int> variable { get; set; }}"
             .Parse(nameof(GetFieldType_System_List)).GetProperties().First().DescendantNodes()
-            .OfType<GenericNameSyntax>().First().GetTType().ToString());
+            .OfType<GenericNameSyntax>().First().GetType(nameof(GetFieldType_System_List)).ToString());
 
     [Fact]
     public void GetFieldType_Custom() => Assert.Equal("Test.MyNameSpace.TestType",
-                       new[] { "using Test.MyNameSpace; using System; class Test2{public TestType variable { get; set; }}",
-                    "using System; namespace Test.MyNameSpace { class TestType{}" }
-                       .Parse("a").First().GetProperties().First().DescendantNodes()
-                       .OfType<IdentifierNameSyntax>().First().GetTType().ToString());
+                       (new[] { "using Test.MyNameSpace; using System; class Test2{public TestType variable { get; set; }}",
+                    "using System; namespace Test.MyNameSpace { class TestType{}" })
+                       .Parse(nameof(GetFieldType_Custom)).First().GetProperties().First().DescendantNodes()
+                       .OfType<IdentifierNameSyntax>().First().GetType(nameof(GetFieldType_Custom)).ToString());
 
     [Fact]
     public void GetFieldType_File2()
@@ -57,7 +57,7 @@ public class FindSyntaxTests
         SyntaxNodeExtensions.SetSemanticModel(1, nameof(GetFieldType_File2));
         Assert.Equal("System.DateTime",
                        tree.GetProperties().First().DescendantNodes()
-                       .OfType<IdentifierNameSyntax>().First().GetTType(nameof(GetFieldType_File2)).ToString());
+                       .OfType<IdentifierNameSyntax>().First().GetType(nameof(GetFieldType_File2)).ToString());
     }
 
     [Fact]
