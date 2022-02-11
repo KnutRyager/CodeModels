8cf4ca1a84738c21df52eb87a101d56c08c0cac4
Knut Ryager <e-k-nut@hotmail.com>
2022-02-11 01:17:41 +0100

    ThisExpression

---
+++ Generator/Models/Primitives/Expression/ThisExpression.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace CodeAnalyzation.Models;
+
+public record ThisExpression(IType Type) : Expression<ThisExpressionSyntax>(Type)
+{
+    public override ThisExpressionSyntax Syntax() => ThisExpression();
+
+    public override IEnumerable<ICodeModel> Children()
+    {
+        yield return Type;
+    }
+
+    public override IExpression Evaluate(IProgramModelExecutionContext context) => context.This();
+}
