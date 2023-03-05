7244e389b5fbb908abba8b0f318ff7b33df8bebd
E-K-n <e-k-nut@hotmail.com>
2023-03-05 20:31:13 +0100

    More libraries

--- Generator/Compilation/Libraries.cs
+++ Generator/Compilation/Libraries.cs
@@ -15,13 +15,17 @@ public static class Libraries
     public static PortableExecutableReference Linq = GetReference(typeof(Enumerable));
     public static PortableExecutableReference IO = GetReference(typeof(File));
     public static PortableExecutableReference Text = GetReference(typeof(System.Text.Encoder));
+    public static PortableExecutableReference RegularExpressions = GetReference(typeof(System.Text.RegularExpressions.Match));
     public static PortableExecutableReference Net = GetReference<HttpClient>();
+    public static PortableExecutableReference StringManipulation = GetReference(typeof(System.String));
     public static PortableExecutableReference Threading = GetReference(typeof(System.Threading.Barrier));
     public static PortableExecutableReference Task = GetReference(typeof(System.Threading.Tasks.Task));
     public static PortableExecutableReference Console = GetReference(typeof(Console));
     public static PortableExecutableReference Xml = GetReference(typeof(System.Xml.ConformanceLevel));
     public static PortableExecutableReference Security = GetReference(typeof(System.Security.AllowPartiallyTrustedCallersAttribute));
+    public static PortableExecutableReference X509Store = GetReference(typeof(System.Security.Cryptography.X509Certificates.X509Store));
     public static PortableExecutableReference Collections = GetReference(typeof(List<>));
+    public static PortableExecutableReference CollectionsGeneric = GetReference(typeof(System.Collections.Generic.List<>));
     public static PortableExecutableReference Concurrent = GetReference(typeof(ConcurrentDictionary<,>));
     public static PortableExecutableReference Numerics = GetReference(typeof(System.Numerics.BigInteger));
 
@@ -31,12 +35,15 @@ public static class Libraries
         Linq,
         IO,
         Text,
+        RegularExpressions,
         Net,
+        StringManipulation,
         Threading,
         Task,
         Console,
         Xml,
         Security,
+        X509Store,
         Collections,
         Concurrent,
         Numerics
