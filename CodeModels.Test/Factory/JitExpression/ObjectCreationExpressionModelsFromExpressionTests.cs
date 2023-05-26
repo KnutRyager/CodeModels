6e57abdd2582f1c5f67c84ae570b98478106c982
E-K-n <e-k-nut@hotmail.com>
2023-05-26 23:44:44 +0200

    SourceGeneration.csproj: Build fix. Array creation source generation test fix.

--- CodeModels.Test/Factory/JitExpression/ObjectCreationExpressionModelsFromExpressionTests.cs
+++ CodeModels.Test/Factory/JitExpression/ObjectCreationExpressionModelsFromExpressionTests.cs
@@ -9,5 +9,10 @@ public class ObjectCreationExpressionModelsFromExpressionTests
 {
     [Fact] public void Object() => CodeModelsFromExpression.GetCode(x => new StringBuilder()).Should().Be("new StringBuilder()");
     [Fact] public void ObjectWithParameters() => CodeModelsFromExpression.GetCode(x => new string("hi")).Should().Be("new string(\"hi\")");
-    [Fact] public void Array() => CodeModelsFromExpression.GetCode(x => new int[] { 1, 2, 3 }).Should().Be("new int[]{1, 2, 3}");
+    [Fact] public void Array() => CodeModelsFromExpression.GetCode(x => new int[] { 1, 2, 3 }).Should().Be(@"new int[]
+{
+    1,
+    2,
+    3
+}");
 }
