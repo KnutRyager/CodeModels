eea6948c741776643c82b1ae8bcf5abf361b4bcb
E-K-n <e-k-nut@hotmail.com>
2023-07-03 00:40:28 +0200

    Type predicate for ReflectionPropertySearch

--- Common.Test/Reflection/ReflectionPropertySearchTests.cs
+++ Common.Test/Reflection/ReflectionPropertySearchTests.cs
@@ -9,14 +9,14 @@ public class ReflectionPropertySearchTests
 {
     [Fact]
     public void FindPath_ObjectToString()
-        => ReflectionPropertySearch.FindPath(typeof(object), typeof(string))
+        => ReflectionPropertySearch.FindPathLimitNamespace(typeof(object), typeof(string))
         .Should().BeEquivalentTo(new List<IMemberAccess>() {
             MemberAccessFactory.Create(ExpressionReflectionUtil.GetMethodInfo(x => x.ToString)) },
             options => options.RespectingRuntimeTypes());
 
     [Fact]
     public void FindPath_Properties_ABC()
-        => ReflectionPropertySearch.FindPath(typeof(A), typeof(C))
+        => ReflectionPropertySearch.FindPathLimitNamespace(typeof(A), typeof(C))
         .Should().BeEquivalentTo(new List<IMemberAccess>() {
             MemberAccessFactory.Create(ExpressionReflectionUtil.GetPropertyInfo<A>(x => x.B).GetMethod),
             MemberAccessFactory.Create(ExpressionReflectionUtil.GetPropertyInfo<B>(x => x.C).GetMethod) },
@@ -24,21 +24,21 @@ public class ReflectionPropertySearchTests
 
     [Fact]
     public void FindPath_Interface()
-        => ReflectionPropertySearch.FindPath(typeof(Garage), typeof(ICar))
+        => ReflectionPropertySearch.FindPathLimitNamespace(typeof(Garage), typeof(ICar))
         .Should().BeEquivalentTo(new List<IMemberAccess>() {
             MemberAccessFactory.Create(ExpressionReflectionUtil.GetPropertyInfo<Garage>(x => x.Car).GetMethod) },
             options => options.RespectingRuntimeTypes());
 
     [Fact]
     public void FindPath_Interface_General()
-        => ReflectionPropertySearch.FindPath(typeof(Garage), typeof(IVehicle))
+        => ReflectionPropertySearch.FindPathLimitNamespace(typeof(Garage), typeof(IVehicle))
         .Should().BeEquivalentTo(new List<IMemberAccess>() {
             MemberAccessFactory.Create(ExpressionReflectionUtil.GetPropertyInfo<Garage>(x => x.Car).GetMethod) },
             options => options.RespectingRuntimeTypes());
 
     [Fact]
     public void FindPath_Interface_Illegal_General()
-        => ReflectionPropertySearch.FindPath(typeof(VehicleStore), typeof(IBoat))
+        => ReflectionPropertySearch.FindPathLimitNamespace(typeof(VehicleStore), typeof(IBoat))
         .Should().BeNull();
 }
 
--- Common/Reflection/ReflectionPropertySearch.cs
+++ Common/Reflection/ReflectionPropertySearch.cs
@@ -1,29 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 using Common.Algorithms.Search;
 using Common.Reflection.Member;
-using Common.Util;
 
 namespace Common.Reflection;
 
 public static class ReflectionPropertySearch
 {
-    public static IMemberAccess? GetMemberAccess(Type start, Type end)
-        => FindPath(start, end) is List<IMemberAccess> access ? MemberAccessFactory.Create(access) : null;
+    public static IMemberAccess? GetMemberAccess(Type start, Type end, Predicate<Type>? typeRestriction = null)
+        => FindPath(start, end, typeRestriction) is List<IMemberAccess> access ? MemberAccessFactory.Create(access) : null;
 
-    public static List<IMemberAccess>? FindPath(Type start, Type end)
+    public static IMemberAccess? GetMemberAccessLimitNamespace(Type start, Type end)
+        => GetMemberAccess(start, end, TypeRestrictions.LimitNamespace(start, end));
+
+    public static List<IMemberAccess>? FindPath(Type start, Type end, Predicate<Type>? typeRestriction = null)
         => start == end ? new List<IMemberAccess>()
         : BreathFirstSearch.FindPathWithEdges(start, end.IsAssignableFrom,
                 x => x.GetMembers().Where(x => x is not (ConstructorInfo or Type or EventInfo)
+                && (typeRestriction is null || typeRestriction.Invoke(ReflectionUtil.GetType(x)))
                 // && x.Name is not ("GetHashCode" or "Compare" or "CompareOrdinal" or "CompareTo")
                 ).ToArray()
             .Select(y => MemberAccessFactory.Create(y)).ToArray(), x => x.Type())
             ?.Skip(1).Select(x => x.Edge).ToList();
+
+    public static List<IMemberAccess>? FindPathLimitNamespace(Type start, Type end)
+        => FindPath(start, end, TypeRestrictions.LimitNamespace(start, end));
+
+    public class TypeRestrictions
+    {
+        public static Predicate<Type> LimitNamespace(Type start, Type end)
+            => x => x.Namespace == start.Namespace || x.Namespace == end.Namespace;
+    }
 }
